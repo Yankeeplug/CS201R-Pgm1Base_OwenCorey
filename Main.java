d166c9c0f7d0d9a17f554a04ec9fa0c38f9dd5e9
Yankeeplug <68196291+Yankeeplug@users.noreply.github.com>
2026-09-06 21:03:06 -0500

    Final commit

--- Main.java
+++ Main.java
@@ -27,14 +27,40 @@ public class Main {
     //TASK 2:
     //add code to input a new value for the variable 'letter'
     //'letter' is defined in line 18
-    
+    System.out.println("Please enter your grade.");
+    String tempString = scanner.next();
+    char tempLetter = tempString.charAt(0);
+    letter = Character.toUpperCase(tempLetter);
+    //System.out.println(letter);
 
     //TASK 3:
-    //if the letter entered is A or B - print "You did GREAT!"
-    //if the letter is C - print "You did GOOD!"
-    //if the letter is D or F - print "Try again"
-    //if the letter is something else, print "Incorrect value"
 
+    char aGrade = 'A';
+    char bGrade = 'B';
+    char cGrade = 'C';
+    char dGrade = 'D';
+    char fGrade = 'F';
+
+    //if the letter entered is A or B - print "You did GREAT!"
+    if (letter == aGrade || letter == bGrade)
+    {
+        System.out.println("You did GREAT!");
+    }
+    //if the letter is C - print "You did GOOD!"
+    else if (letter == cGrade)
+    {
+        System.out.println("You did GOOD!");
+    }
+    //if the letter is D or F - print "Try again"
+    else if (letter == dGrade || letter == fGrade)
+    {
+        System.out.println("Try again");
+    }
+    //if the letter is something else, print "Incorrect value"
+    else
+    {
+        System.out.println("Incorrect value");
+    }
 
     //LECTURE 1 DEMO:
     //this is demonstrating different values
